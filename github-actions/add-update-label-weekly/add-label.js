374a7e41698d715fd0227b0c080feaba045a40bd
Bitian Zhang <86077274+bzzz-coding@users.noreply.github.com>
2023-03-09 10:37:26 -0800

    Changed interval back to days for testing

--- github-actions/add-update-label-weekly/add-label.js
+++ github-actions/add-update-label-weekly/add-label.js
@@ -12,11 +12,11 @@ const inactiveLabel = '2 weeks inactive'; // If the last time an issue was cross
 
 const commentByDays = 7; // number of days ago to check for toUpdateLabel
 const sevenDayCutoffTime = new Date()
-sevenDayCutoffTime.setHours(sevenDayCutoffTime.getHours() - commentByDays)
+sevenDayCutoffTime.setDays(sevenDayCutoffTime.getDays() - commentByDays)
 
 const inactiveUpdatedByDays = 14; // number of days ago to check for inactiveLabel
 const fourteenDayCutoffTime = new Date()
-fourteenDayCutoffTime.setHours(fourteenDayCutoffTime.getHours() - inactiveUpdatedByDays)
+fourteenDayCutoffTime.setDays(fourteenDayCutoffTime.getDays() - inactiveUpdatedByDays)
 
 /**
  * The main function, which retrieves issues from a specific column in a specific project, before examining the timeline of each issue for outdatedness. If outdated, the old status label is removed, and an updated is requested. Otherwise, the issue is labeled as updated.
